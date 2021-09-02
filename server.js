cbd7cd7fdf3463f03879bdb731d61e5de789a400
Sabrina Holanda <maria.sholanda@a.ficr.edu.com>
2021-09-02 00:45:57 -0300

    criacao no server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 const port = process.env.PORT || 3003 
 
 app.get('/', (req, res) => {      
-    return res.send(home.vue)
+    return res.redirect('home')
      })
 app.listen(port)
