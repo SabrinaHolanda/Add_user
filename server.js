5b871b0fb6d20176341bef697ec7916f772f882b
Sabrina Holanda <maria.sholanda@a.ficr.edu.com>
2021-09-10 00:17:05 -0300

    add nodemon .env

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const env = require('dotenv')
+env.config()
+
+//importa o express
 const express = require('express')
+//cria o app do tipo express()
 const app = express()
 
+//define a porta do app como a do ambiente ou 3003
 const port = process.env.PORT || 3003 
 
+//cria uma rota na raiz do endereço da aplicação que retorna uma mensagem.
 app.get('/', (req, res) => {      
     return res.send('API RODANDO COM SUCESSO!')
      })
-app.listen(port)
+//coloca a plaicação para rodar uma porta definida
+app.listen(port, () => {
+    console.log('Funcionando na porta', port)
+})
