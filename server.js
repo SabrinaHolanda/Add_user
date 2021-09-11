069aa50c6a2a744371ed84cc8bb55657f588ae70
Sabrina Holanda <maria.sholanda@a.ficr.edu.com>
2021-09-10 23:59:08 -0300

    package server

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const express = require('express')
 //cria o app do tipo express()
 const app = express()
 
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_Name, process.env.DB_User, process.env.DB_Password, {
+    host: process.env.DB_Host,
+    dialect: 'mssql'
+  });
+
+  try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 //define a porta do app como a do ambiente ou 3003
 const port = process.env.PORT || 3003 
 
